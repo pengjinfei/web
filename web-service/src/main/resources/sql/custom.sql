0e02a627dff33fb4ed09f8bb39709452863f2f80
pengjinfei <pengjinfei@126.com>
2016-03-31 14:43:03 +0800

    add table custom

---
+++ web-service/src/main/resources/sql/custom.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `custom` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `create_date` datetime DEFAULT NULL,
+  `update_date` datetime DEFAULT NULL,
+  `name` varchar(50) NOT NULL,
+  `age` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8
