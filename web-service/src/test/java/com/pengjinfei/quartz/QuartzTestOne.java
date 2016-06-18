9aad29eb27d91c7c9ac54093c8ba62e207ba7481
pengjinfei <pengjinfei@126.com>
2016-06-18 19:29:08 +0800

    change datasource

---
+++ web-service/src/test/java/com/pengjinfei/quartz/QuartzTestOne.java
@@ -0,0 +1,15 @@
+package com.pengjinfei.quartz;
+
+import org.junit.Test;
+
+/**
+ * Created by Pengjinfei on 16/6/18.
+ * Description:
+ */
+public class QuartzTestOne {
+
+    @Test
+    public void testQuick() {
+    }
+
+}
