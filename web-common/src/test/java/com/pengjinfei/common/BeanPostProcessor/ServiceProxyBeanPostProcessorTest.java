de068b551e53860a1b79457f6f6eca72b07b3368
pengjinfei <pengjinfei@126.com>
2016-07-04 21:37:53 +0800

    test zookeeper

--- web-common/src/test/java/com/pengjinfei/common/BeanPostProcessor/ServiceProxyBeanPostProcessorTest.java
+++ web-common/src/test/java/com/pengjinfei/common/BeanPostProcessor/ServiceProxyBeanPostProcessorTest.java
@@ -1,19 +1,17 @@
 package com.pengjinfei.common.BeanPostProcessor;
 
-import com.pengjinfei.common.lock.impl.ZookeeperPreemptiveLock;
-import org.apache.zookeeper.*;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Author: EX-PENGJINFEI001
@@ -34,48 +32,15 @@ public class ServiceProxyBeanPostProcessorTest {
 
     @Test
     public void testZookeeper() throws Exception{
-        ZooKeeper zk = new ZooKeeper("127.0.0.1:2181",
-                3000, new Watcher() {
-            // 监控所有被触发的事件
-            public void process(WatchedEvent event) {
-                System.out.println("已经触发了" + event.getType() + "事件！");
-            }
-        });
-        // 创建一个目录节点
-        zk.delete("/testRootPath",-1);
-        zk.create("/test", "good".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE,
-                CreateMode.EPHEMERAL);
-        Thread.sleep(5000);
-    }
-
-    @Test
-    public void testSpring() throws IOException {
-        InputStream resourceAsStream = ZookeeperPreemptiveLock.class.getClassLoader().getResourceAsStream("common.properties");
-        Properties properties = new Properties();
-        properties.load(resourceAsStream);
-        String url = resolvePlaceholder("host",properties);
-        System.out.println(url);
-    }
-
-    private static String resolvePlaceholder(String placeholder, Properties props) {
-        String property = props.getProperty(placeholder);
-        Matcher matcher = PATTERN.matcher(property);
-        StringBuffer buffer = new StringBuffer();
-        while (matcher.find()) {
-            String matcherKey = matcher.group(1);
-            String matchervalue = props.getProperty(matcherKey);
-            if (matchervalue != null) {
-                matcher.appendReplacement(buffer, matchervalue);
-            }
+        String zookeeperUrl = "120.25.95.166:2181";
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient(zookeeperUrl, retryPolicy);
+        curatorFramework.start();
+        List<String> bytes = curatorFramework.getChildren().forPath("/dubbo/com.pengjinfei.core.service.CustomerService/providers");
+        for (String aByte : bytes) {
+            System.out.println(aByte);
         }
-        matcher.appendTail(buffer);
-        return buffer.toString();
     }
-
-    private static final Pattern PATTERN = Pattern
-            .compile("\\$\\{([^\\}]+)\\}");
-
-
 
     @Test
     public void testBlockingQueue() throws InterruptedException {
